e3b2cfaceb53cb5b7d87423d49e9eac399be3860
gabejoseph <gabriel.joseph4@gmail.com>
2020-07-09 17:13:07 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+const input = document.getElementById('input');
+
 function addingEventListener() {
+    input.addEventListener('click', function(event) 
+        {
+        alert('I was clicked!');
+        }
+    );
 }
+
