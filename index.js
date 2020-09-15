7f8c9e59eefb7a5bf7f664f0678e876c64c49020
github username <github email address>
2020-09-15 15:26:30 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 function addingEventListener() {
+
+    const input = document.getElementById('input');
+input.addEventListener('click', function(event) {
+  alert('I was clicked!');
+});
+
 }
+
+
+//takes in two arguments 
+//name of the event, callback function 
+//node that will listen: input constant 
+//invoking addEventListener on the node will listen 
+//first argument: event name to listen for 
+//second argument: callback function, work that will be executed when the node hears the event 
