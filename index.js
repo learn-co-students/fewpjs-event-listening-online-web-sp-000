dd0d43880dedff1950e8ba7f724d409136628f44
Lj <tnedutsl457892@gmail.com>
2021-06-30 21:26:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 function addingEventListener() {
+  const input = document.getElementById('input');
+  //   event name to listen for, callback function 
+  input.addEventListener('click', function(event) {
+    alert('I was clicked!');
+  });
 }
